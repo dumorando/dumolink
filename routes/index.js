4b4a063e3da17145def3d749f3bcbbcda8d905f6
dumorando <dumorando@proton.me>
2024-07-15 23:55:05 -0700

    add redirect

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var createError = require('http-errors');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Home' });
+});
+
+/* redirect to a url from the id. */
+router.get('/:id', async function(req, res, next) {
+  /**
+   * @type {import("quick.db").QuickDB}
+   */
+  var db = req.app.locals.db;
+
+  if (!await db.has(req.params.id)) {
+    next(createError(404));
+  }
+
+  res.redirect(await db.get(req.params.id));
 });
 
 module.exports = router;
